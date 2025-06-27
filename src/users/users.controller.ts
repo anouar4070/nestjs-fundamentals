9e970e88d78bdd0bdc95bd59e8d4ad54f31be2ed
anouar garbaa <g.anouar@yahoo.com>
2025-06-27 19:25:12 +0200

    add user service layer

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,7 +10,6 @@ import {
   ParseUUIDPipe,
   Patch,
   Post,
-  Query,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
